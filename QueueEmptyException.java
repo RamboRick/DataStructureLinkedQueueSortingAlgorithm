54ceaac442e3fb01022d43b53e6c044c6991b2f1
Fanghao(Gabriel) Song <fanghaos2014@gmail.com>
2018-03-28 17:56:39 -0400

    Add files via upload

---
+++ QueueEmptyException.java
@@ -0,0 +1,15 @@
+/* QueueEmptyException.java */
+
+package listhw8;
+
+public class QueueEmptyException extends Exception {
+
+  public QueueEmptyException() {
+    super();
+  }
+
+  public QueueEmptyException(String s) {
+    super(s);
+  }
+
+}
